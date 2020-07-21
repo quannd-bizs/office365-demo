2b9b56240bf86ef36490b6585921be63ddf97a52
quannd <quan.nguyenduc@gmail.com>
2020-07-21 07:09:18 +0700

    add function

---
+++ app/services/office365_service.rb
@@ -0,0 +1,9 @@
+class Office365Service
+    attr_accessor :email
+  
+    SENDMAIL_ENDPOINT = "/v1.0/me/microsoft.graph.sendmail"
+    CLIENT_CRED = ADAL::ClientCredential.new(
+      ENV["CLIENT_ID"], ENV["CLIENT_SECRET"])
+  
+    def initialize
+    end
